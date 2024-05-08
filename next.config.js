620d990c149bb88412cea6d673efc34a45542185
Vi-lka <vitalya.permyakov155@gmail.com>
2024-05-08 20:09:18 +0700

    start proj

---
+++ next.config.js
@@ -0,0 +1,31 @@
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+  output: "standalone",
+  reactStrictMode: true,
+  eslint: { ignoreDuringBuilds: true },
+  env: {
+    // Reference a variable that was defined in the .env file and make it available at Build Time
+    NEXT_PUBLIC_URL: process.env.NEXT_PUBLIC_URL,
+    NEXT_PUBLIC_STRAPI_API_URL: process.env.NEXT_PUBLIC_STRAPI_API_URL,
+    NEXT_PUBLIC_STRAPI_DOMAIN: process.env.NEXT_PUBLIC_STRAPI_DOMAIN,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: process.env.NEXT_PUBLIC_STRAPI_DOMAIN,
+      },
+    ],
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/uploads/:path*",
+        destination: `${process.env.NEXT_PUBLIC_STRAPI_API_URL}/uploads/:path*`,
+      },
+    ];
+  },
+};
+
+export default nextConfig;
